1d974a81ea3fadd199e845e5bfc4add2adfdc789
CoderCat010 <nusraumme@gmail.com>
2026-01-17 12:12:02 +0600

    added practice-15

--- string-array-practice-problems.js
+++ string-array-practice-problems.js
@@ -7,3 +7,14 @@
 //     'e'
 // ]
 // console.log(arrStr);
+
+
+//-----> Find the length of each string in an array.
+let arrStr = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e'
+];
+console.log(arrStr.length);
